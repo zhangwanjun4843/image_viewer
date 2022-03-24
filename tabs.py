5086cefac70b7f2f1efb888e7e4a89b6a4bf7d06
Paul Thorand <paulthorand.2005@gmail.com>
2022-03-24 21:16:46 +1030

    added class and resizing of image

--- tabs.py
+++ tabs.py
@@ -1,34 +1,45 @@
-
 import sys, os
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
 from PySide6.QtUiTools import QUiLoader
 from PySide6 import QtGui
 from PySide6.QtCore import Qt
 
 from qt_material import apply_stylesheet
 
-def show_screenshot(window):
-    img_path = window.lineEdit.text()
-    if not os.path.isfile(img_path):
-        window.label.setText(f"{img_path}\nis not a valid image path")
-        return
 
-    pixmap = QtGui.QPixmap(img_path)
-    scaled_pixmap = pixmap.scaled(window.label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-    window.label.setPixmap(scaled_pixmap)
 
-def resize(window, event):
-    print("resized")
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        loader = QUiLoader()
+        self.ui = loader.load("tabs.ui", None)
+        
+        self.ui.pushButton.clicked.connect(lambda: self.show_screenshot())
+
+        self.setCentralWidget(self.ui)
+
+    def show_screenshot(self):
+        img_path = self.ui.lineEdit.text()
+        if not os.path.isfile(img_path):
+            self.ui.label.setText(f"{img_path}\nis not a valid image path")
+            return
+
+        pixmap = QtGui.QPixmap(img_path)
+        scaled_pixmap = pixmap.scaled(self.ui.label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.ui.label.setPixmap(scaled_pixmap)
+
+    def resizeEvent(self, event):
+        self.show_screenshot()
+
+        return super().resizeEvent(event)
+
 
 app = QApplication(sys.argv)
-
-loader = QUiLoader()
-window = loader.load("tabs.ui", None)
-
-window.pushButton.clicked.connect(lambda: show_screenshot(window))
+window = MainWindow()
 
 apply_stylesheet(app, theme='dark_teal.xml')
 
-window.show()
+window.showMaximized()
 app.exec()
