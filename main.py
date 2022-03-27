5ecc85013448740a6f7c3b7daa81e49aa21ca542
MergenStudios <paulthorand.2005@gmail.com>
2022-03-27 22:34:10 +1030

    added flip button

--- main.py
+++ main.py
@@ -18,18 +18,16 @@ class MainWindow(QMainWindow):
         self.ui.imageViewer.setStyleSheet("border-width: 0px; border-style: solid")
 
         self.ui.pushButton.clicked.connect(lambda: self.show_image())
-        self.ui.actionSave.triggered.connect(lambda: pprint.pprint())
-        
+        self.ui.pushButton_2.clicked.connect(lambda: self.flip_image())
+
         self.ui.verticalLayout_2.addWidget(self.ui.imageViewer)
         self.setCentralWidget(self.ui)
 
     def show_image(self):
-        img_path = self.ui.lineEdit.text()
-        if not os.path.isfile(img_path):
-            print(f"{img_path} is not a valid path")
+        self.ui.imageViewer.loadImageFromFile()
 
-        self.ui.imageViewer.loadImageFromFile(img_path)
-
+    def flip_image(self):
+        self.ui.imageViewer.scale(-1, 1)
 
 if __name__ == '__main__':
 
